8d5a238560decb284d287d643ad80637c410a8a0
helen-tan <helentanmz0909@gmail.com>
2022-12-22 14:59:43 +0800

    Created 2 api routes

---
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/controller/StudentController.java
@@ -0,0 +1,46 @@
+package com.springbootgs.springbootgradesubmissionapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springbootgs.springbootgradesubmissionapi.entity.Student;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    
+    // GET a student by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // POST - Create a student
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) { // de-serialize payload from req from json to a student java obj (new obj created here) with @RequestBody
+        // Call Service method
+
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    // DELETE a student by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // GET all students
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
