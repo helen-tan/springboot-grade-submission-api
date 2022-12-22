44541ec6167c6ad9d77ea07dcd046eca4d2e47c5
helen-tan <helentanmz0909@gmail.com>
2022-12-22 16:16:58 +0800

    Created api routes for course

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/controller/CourseController.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/controller/CourseController.java
@@ -1,11 +1,43 @@
 package com.springbootgs.springbootgradesubmissionapi.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springbootgs.springbootgradesubmissionapi.entity.Course;
 
 @RestController
 @RequestMapping("/course")
 public class CourseController {
+    // GET a course by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
+    // POST - Create a course
+    @PostMapping
+    public ResponseEntity<Course> saveCourse(@RequestBody Course course) {
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
+    }
 
+    // DELETE a course by course id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // GET all courses
+    @GetMapping("/all")
+    public ResponseEntity<List<Course>> getCourses() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
