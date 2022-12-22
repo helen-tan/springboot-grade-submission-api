bd6ed7d400e92c6d45825b970418b2fcc99d9128
helen-tan <helentanmz0909@gmail.com>
2022-12-22 10:49:30 +0800

    New Spring Boot project

---
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/SpringbootGradeSubmissionApiApplication.java
@@ -0,0 +1,13 @@
+package com.springbootgs.springbootgradesubmissionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradeSubmissionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradeSubmissionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootgs/spingbootgradesubmissionapi/SpingbootGradeSubmissionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootgs.spingbootgradesubmissionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootGradeSubmissionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
