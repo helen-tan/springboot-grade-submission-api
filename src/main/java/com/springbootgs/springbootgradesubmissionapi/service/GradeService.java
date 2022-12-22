d36b453d192b765db6d0382b164bd398cb8e7e40
helen-tan <helentanmz0909@gmail.com>
2022-12-22 11:44:34 +0800

    Grade pojo, controller, repository, service starter

---
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/service/GradeService.java
@@ -0,0 +1,15 @@
+package com.springbootgs.springbootgradesubmissionapi.service;
+
+import java.util.List;
+
+import com.springbootgs.springbootgradesubmissionapi.entity.Grade;
+
+public interface GradeService {
+    Grade getGrade(Long studentId, Long courseId);
+    Grade saveGrade(Grade grade, Long studentId, Long courseId);
+    Grade updateGrade(String score, Long studentId, Long courseId);
+    void deleteGrade(Long studentId, Long courseId);
+    List<Grade> getStudentGrades(Long studentId);
+    List<Grade> getCourseGrades(Long courseId);
+    List<Grade> getAllGrades();
+}
---
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/service/GradeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.springbootgs.springbootgradesubmissionapi.service;
+
+import java.util.List;
+
+import com.springbootgs.springbootgradesubmissionapi.entity.Grade;
+
+public class GradeServiceImpl implements GradeService {
+    
+    @Override
+    public Grade getGrade(Long studentId, Long courseId) {
+        return null;
+    }
+
+    @Override
+    public Grade saveGrade(Grade grade, Long studentId, Long courseId) {
+        return null;
+    }
+
+    @Override
+    public Grade updateGrade(String score, Long studentId, Long courseId) {
+        return null;
+    }
+
+    @Override
+    public void deleteGrade(Long studentId, Long courseId) {
+        
+    }
+
+    @Override
+    public List<Grade> getStudentGrades(Long studentId) {
+        return null;
+    }
+
+    @Override
+    public List<Grade> getCourseGrades(Long courseId) {
+        return null;
+    }
+
+    @Override
+    public List<Grade> getAllGrades() {
+        return null;
+    }
+
+}
+
