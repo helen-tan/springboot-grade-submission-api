317f0cda1294da2ed2e07e44ede4785f9b417ff9
helen-tan <helentanmz0909@gmail.com>
2022-12-22 17:20:30 +0800

    Create student table in H2 db

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Student.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Student.java
@@ -2,12 +2,25 @@ package com.springbootgs.springbootgradesubmissionapi.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // Telling Spring Boot JPA to create a (student) table that stores student entities 
+@Table(name = "student") // Name the table
 public class Student {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
-
+    
     public Long getId() {
         return this.id;
     }
