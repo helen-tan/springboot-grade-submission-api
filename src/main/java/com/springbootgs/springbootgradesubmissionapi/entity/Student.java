6c7d7885b75fa84430239572b04d48124cfd9b68
helen-tan <helentanmz0909@gmail.com>
2022-12-27 11:59:48 +0800

    Used Lombok @Getter & @Setter to generate getters and setters

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Student.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Student.java
@@ -8,9 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity // Telling Spring Boot JPA to create a (student) table that stores student entities 
 @Table(name = "student") // Name the table
+@Getter // Lombok-enabled - generates getters based on defined fields
+@Setter // Lombok-enabled - generates
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,29 +24,5 @@ public class Student {
     private String name;
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
-    
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return this.birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
 
 }
