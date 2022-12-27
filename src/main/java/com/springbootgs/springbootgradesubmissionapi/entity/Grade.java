5ba727d07bcdb555dc937cb4145c1a56f88b168c
helen-tan <helentanmz0909@gmail.com>
2022-12-27 16:41:53 +0800

    Create Grade entity & table

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Grade.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Grade.java
@@ -1,14 +1,26 @@
 package com.springbootgs.springbootgradesubmissionapi.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
+@Entity
+@Table(name = "grade")
 @Getter // Lombok-enabled - generates getters based on defined fields
 @Setter // Lombok-enabled - generates
 @AllArgsConstructor
 @NoArgsConstructor
 public class Grade {
 
+    @Id // this column is primary key - values uniquely identify a grade
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // every id is auto-generated
+    @Column(name = "id")
     private Long id;
+    @Column(name = "score")
     private String score;
 
 }
