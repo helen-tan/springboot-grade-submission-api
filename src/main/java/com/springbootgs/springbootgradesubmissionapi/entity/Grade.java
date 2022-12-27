c4c8412eda79c5a5c97714ad04eb572852b113ba
helen-tan <helentanmz0909@gmail.com>
2022-12-27 17:10:16 +0800

    Create Many-to-one relationship between student & grade table

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Grade.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Grade.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -23,4 +25,9 @@ public class Grade {
     @Column(name = "score", nullable = false)
     private String score;
 
+    // Tell Spring Boot that there will be a many-to-one relationship, as Spring JPA creates the grade table
+    // Many grades will be associated with one student. Child table will manage the foreign key
+    @ManyToOne 
+    @JoinColumn(name = "student_id", referencedColumnName = "id") // Define foreign key column (needs to reference the primary key of another table)
+    private Student student;
 }
