9fe58aef637937d950157bfef9a2a688a50e38c6
helen-tan <helentanmz0909@gmail.com>
2022-12-28 16:58:26 +0800

    Setup Course entity

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Course.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/Course.java
@@ -1,16 +1,36 @@
 package com.springbootgs.springbootgradesubmissionapi.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
+@Entity
+@Table(name = "course")
 @Getter // Lombok-enabled - generates getters based on defined fields
-@Setter // Lombok-enabled - generates
-@AllArgsConstructor
+@Setter // Lombok-enabled - generates setters
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @NonNull
+    @Column(name = "subject", nullable = false)
     private String subject;
+
+    @NonNull
+    @Column(name = "code", nullable = false)
     private String code;
+
+    @NonNull
+    @Column(name = "description", nullable = false)
     private String description;
 
 }
