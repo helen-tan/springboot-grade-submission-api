7043182272b7026b10db81c54d291732f2593d2f
helen-tan <helentanmz0909@gmail.com>
2022-12-29 12:03:15 +0800

    Using Optionals to warn of a possible null value returned

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/repository/GradeRepository.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/repository/GradeRepository.java
@@ -1,9 +1,14 @@
 package com.springbootgs.springbootgradesubmissionapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.springbootgs.springbootgradesubmissionapi.entity.Grade;
 
 public interface GradeRepository extends CrudRepository<Grade, Long>{
-    Grade findByStudentIdAndCourseId(Long studentId, Long courseId);
+    // Returns a Grade wrapped around an Optional
+    // This warns whoever that is calling the method that the object sent back is at risk of being null
+    // By wrapping the object in an Optional, the caller will not get a Null pointer exception, and can anticipate this scenario and handle the Optional
+    Optional<Grade> findByStudentIdAndCourseId(Long studentId, Long courseId);
 }
--- src/main/java/com/springbootgs/springbootgradesubmissionapi/service/GradeServiceImpl.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/service/GradeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springbootgs.springbootgradesubmissionapi.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.springbootgs.springbootgradesubmissionapi.entity.Course;
 import com.springbootgs.springbootgradesubmissionapi.entity.Grade;
 import com.springbootgs.springbootgradesubmissionapi.entity.Student;
+import com.springbootgs.springbootgradesubmissionapi.exception.GradeNotFoundException;
 import com.springbootgs.springbootgradesubmissionapi.repository.CourseRepository;
 import com.springbootgs.springbootgradesubmissionapi.repository.GradeRepository;
 import com.springbootgs.springbootgradesubmissionapi.repository.StudentRepository;
@@ -26,7 +28,13 @@ public class GradeServiceImpl implements GradeService {
 
     @Override
     public Grade getGrade(Long studentId, Long courseId) {
-        return gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
+        Optional<Grade> grade = gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
+
+        if (grade.isPresent()) {
+            return grade.get();
+        } else {
+            throw new GradeNotFoundException(studentId, courseId);
+        }
     }
 
     @Override
@@ -44,10 +52,17 @@ public class GradeServiceImpl implements GradeService {
     @Override
     public Grade updateGrade(String score, Long studentId, Long courseId) {
         // Fetch a grade that already exists
-        Grade grade = gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
-        // Update the grade
-        grade.setScore(score);
-        return gradeRepository.save(grade);
+        Optional<Grade> grade = gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
+        // If grade does not exist
+        if (grade.isPresent()) {
+            Grade unwrappedGrade = grade.get(); // Unwrap Optional
+            // Update the grade object
+            unwrappedGrade.setScore(score);
+            // Save into DB
+            return gradeRepository.save(unwrappedGrade);
+        } else {
+            throw new GradeNotFoundException(studentId, courseId);
+        }
     }
 
     @Override
