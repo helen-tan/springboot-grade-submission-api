8d0ed7697351aa429010fb53500c0b860479086b
helen-tan <helentanmz0909@gmail.com>
2022-12-29 11:51:50 +0800

    throw exception when course not found

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/service/CourseServiceImpl.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/service/CourseServiceImpl.java
@@ -1,11 +1,13 @@
 package com.springbootgs.springbootgradesubmissionapi.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springbootgs.springbootgradesubmissionapi.entity.Course;
+import com.springbootgs.springbootgradesubmissionapi.exception.CourseNotFoundException;
 import com.springbootgs.springbootgradesubmissionapi.repository.CourseRepository;
 
 @Service
@@ -16,7 +18,13 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public Course getCourse(Long id) {
-        return courseRepository.findById(id).get();
+        Optional<Course> course = courseRepository.findById(id); 
+
+        if (course.isPresent()) {
+            return course.get();
+        } else {
+            throw new CourseNotFoundException(id);
+        }
     }
 
     @Override
