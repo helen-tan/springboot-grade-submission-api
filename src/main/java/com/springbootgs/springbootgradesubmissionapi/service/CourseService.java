18a926d6aab24a681d59ef80d6bcd1b6a0096fc4
helen-tan <helentanmz0909@gmail.com>
2023-01-03 14:45:43 +0800

    just commit

--- src/main/java/com/springbootgs/springbootgradesubmissionapi/service/CourseService.java
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/service/CourseService.java
@@ -3,7 +3,6 @@ package com.springbootgs.springbootgradesubmissionapi.service;
 import java.util.List;
 
 import com.springbootgs.springbootgradesubmissionapi.entity.Course;
-import com.springbootgs.springbootgradesubmissionapi.entity.Student;
 
 public interface CourseService {
     Course getCourse(Long id);
