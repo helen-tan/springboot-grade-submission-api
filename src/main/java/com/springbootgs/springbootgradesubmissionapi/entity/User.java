c3f4e8c4da4a748d4679bd01f635df68df687b62
helen-tan <helentanmz0909@gmail.com>
2023-01-05 10:05:13 +0800

    Created User Entity

---
+++ src/main/java/com/springbootgs/springbootgradesubmissionapi/entity/User.java
@@ -0,0 +1,46 @@
+package com.springbootgs.springbootgradesubmissionapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+/*
+ * 'user' is a reserved keyword in SQL, so we name our table users. If you name it user, you will get a org.h2.jdbc.JdbcSQLSyntaxErrorException. 
+ *  See https://docs.microsoft.com/en-us/sql/t-sql/language-elements/reserved-keywords-transact-sql?view=sql-server-ver16 for a list of reserved keywords.
+ */ 
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank(message =  "username cannot be blank")
+	@NonNull
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@NotBlank(message =  "password cannot be blank")
+    @NonNull
+	@Column(nullable = false)
+	private String password;
+    
+}
+
+
+
+
